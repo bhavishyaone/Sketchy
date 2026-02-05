2749697c620d582a345ea114bbe1ca6f383f2da4
Bhavishya Sharma <bhavishaya32@gmail.com>
2026-02-05 14:15:00 +0530

    feat: register About route in App router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import Home from './pages/Home'
 import Work from './pages/Work'
+import About from './pages/About'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/work" element={<Work />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </main>
       <Footer />
