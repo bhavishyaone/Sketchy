e3f59528ee88f42594378d497a3efc3b5ba8ba60
Bhavishya Sharma <bhavishaya32@gmail.com>
2026-02-06 16:00:00 +0530

    feat: connect Contact page to main router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Footer from './components/layout/Footer'
 import Home from './pages/Home'
 import Work from './pages/Work'
 import About from './pages/About'
+import Contact from './pages/Contact'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/work" element={<Work />} />
           <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </main>
       <Footer />
