c7c2c95302fd29e62673e4db6b1986bd5d74c748
Bhavishya Sharma <bhavishaya32@gmail.com>
2026-02-07 09:15:00 +0530

    feat(api): define Project Mongoose schema

---
+++ server/src/models/Project.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+
+const projectSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true, // URL to image
+  },
+  description: {
+    type: String,
+  },
+  link: {
+    type: String,
+  }
+}, { timestamps: true })
+
+const Project = mongoose.model('Project', projectSchema)
+export default Project
