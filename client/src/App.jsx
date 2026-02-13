53f7d7794b14d968d1a51e9ef5f64514cb7bd11a
Bhavishya Sharma <bhavishaya32@gmail.com>
2026-02-13 13:15:00 +0530

    feat(web): connect dynamic project detail route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import Home from './pages/Home'
 import Work from './pages/Work'
+import ProjectDetails from './pages/ProjectDetails'
 import About from './pages/About'
 import Contact from './pages/Contact'
 
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/work" element={<Work />} />
+          <Route path="/work/:id" element={<ProjectDetails />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
